d5f0976cd403c343fb0fdc2ac6ae578aae01c8d7
avi <hi@avi.im>
2018-02-04 17:06:55 +0530

    Add settings with allauth configs

---
+++ gob/settings/dev.py
@@ -0,0 +1,9 @@
+from gob.settings.common import * # noqa
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '646ozit$$ez4r&#46cau+)(r8nh#5a#l1a467lymsfytg=)wjd'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []

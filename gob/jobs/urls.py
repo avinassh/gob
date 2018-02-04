ba58f8a795df9a28c95bfad7150c0751b377bb6f
avi <hi@avi.im>
2018-02-04 21:24:14 +0530

    Use app namespaces for URLs

--- gob/jobs/urls.py
+++ gob/jobs/urls.py
@@ -4,9 +4,12 @@ from django.views.generic import RedirectView
 
 from gob.jobs.views import JobListView, JobCreateView
 
+app_name = 'jobs'
 urlpatterns = [
     url(r'^$', JobListView.as_view(), name='jobs'),
+    url(r'^add', JobCreateView.as_view(), name='jobs-add'),
+    # TODO:
+    # Move following to user_manager
     url(r'^login', RedirectView.as_view(
-        url=settings.OAUTH_LOGIN_URL, permanent=False)),
-    url(r'^add', JobCreateView.as_view(), name='jobs-add')
+        url=settings.OAUTH_LOGIN_URL, permanent=False), name='login'),
 ]

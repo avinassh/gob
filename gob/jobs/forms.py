b14bc44d86ec0c0cd148f8832a19697b429313ff
avi <hi@avi.im>
2018-02-04 19:30:51 +0530

    Add `JobCreateView`

---
+++ gob/jobs/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.forms import ModelForm
+
+from gob.jobs.models import Job
+
+
+class JobCreateForm(ModelForm):
+    company_name = forms.CharField(required=False, max_length=80)
+    salary_end = forms.IntegerField(required=False)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if self.errors:
+            return cleaned_data
+        salary_start = cleaned_data['salary_start']
+        salary_end = cleaned_data.get('salary_end')
+        if salary_end and salary_end < salary_start:
+            raise forms.ValidationError(
+                "Invalid salary range",
+                code='invalid_salary',
+            )
+        return cleaned_data
+
+    class Meta:
+        model = Job
+        fields = ['company_name', 'description', 'location', 'salary_start',
+                  'salary_end']
--- gob/jobs/views.py
+++ gob/jobs/views.py
@@ -1,7 +1,20 @@
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 
 from gob.jobs.models import Job
+from gob.jobs.forms import JobCreateForm
 
 
 class JobListView(ListView):
     model = Job
+
+
+class JobCreateView(CreateView):
+    model = Job
+    form_class = JobCreateForm
+
+    def form_valid(self, form):
+        import ipdb; ipdb.set_trace()
+
+    def form_invalid(self, form):
+        import ipdb; ipdb.set_trace()

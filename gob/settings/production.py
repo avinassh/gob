b47682bba6ab0acd8460e8adacc6e8814e699a4a
avi <hi@avi.im>
2018-02-04 23:42:42 +0530

    Add DJ Database URL

--- gob/settings/production.py
+++ gob/settings/production.py
@@ -1,5 +1,7 @@
 import os
 
+import dj_database_url
+
 from gob.settings.common import * # noqa
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -14,3 +16,8 @@ OAUTH_LOGIN_PROVIDER = os.environ['OAUTH_LOGIN_PROVIDER']
 
 if OAUTH_LOGIN_PROVIDER == 'slack':
     SLACK_TEAM_ID = os.environ['SLACK_TEAM_ID']
+
+_db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES = {}
+DATABASES['default'] = {}
+DATABASES['default'].update(_db_from_env)
